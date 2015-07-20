133aa75b23208f0768cd243ceda4a3a7a54aeadc
phelixnmc <githubnmc@blockchained.com>
2015-07-20 10:18:46 +0200

    build windows executable

---
+++ build_windows.bat
@@ -0,0 +1,20 @@
+C:\Python27\Scripts\pyinstaller -p lib;gfx ^
+    --hidden-import=asyncore --hidden-import=win32gui_struct --hidden-import=win32gui ^
+    --hidden-import=winxpgui --hidden-import=commctrl --hidden-import=pywintypes ^
+    --noconfirm  --icon=./gfx/favicon.ico namegui.py
+
+@if %errorlevel% EQU 0 goto continue
+@pause
+:continue
+
+mkdir dist\nameguiwin\lib\
+mkdir dist\nameguiwin\gfx\
+
+xcopy lib dist\nameguiwin\lib /s /e /h /y
+xcopy gfx dist\nameguiwin\gfx /s /e /h /y
+
+rmdir dist\nameguiwin\_MEI\tk\images /s /q
+rmdir dist\nameguiwin\_MEI\tcl\tzdata /s /q
+rmdir dist\nameguiwin\_MEI\tcl\msgs /s /q
+
+del dist\*.pyc /s /q
