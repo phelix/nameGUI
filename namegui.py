73236578b2b859386cd99dc5609468d1e38839e8
phelixnmc <githubnmc@blockchained.com>
2016-01-16 17:02:41 +0100

    move antpy stuff to src folder

--- namegui.py
+++ namegui.py
@@ -3,7 +3,6 @@ import shared  # needs to be imported early because of configuration and cli par
 import sys
 sys.path.append("src")
 sys.path.append("lib")
-sys.path.append("antpy")
 
 import splashscreen
 import os
