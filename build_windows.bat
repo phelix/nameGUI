582049c89c7b11b6b9434bda5741cb3bfd87c73f
phelixnmc <githubnmc@blockchained.com>
2016-01-16 17:05:00 +0100

    build_windows.bat: add src folder

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
-C:\Python27\Scripts\pyinstaller -p lib;gfx ^
+C:\Python27\Scripts\pyinstaller -p lib;gfx;src ^
     --hidden-import=asyncore --hidden-import=win32gui_struct --hidden-import=win32gui ^
     --hidden-import=winxpgui --hidden-import=commctrl --hidden-import=pywintypes ^
-    --noconfirm --icon=./gfx/favicon.ico nameguiwin.pyw --noconsole
+     --noconfirm --noconsole --icon=./gfx/favicon.ico nameguiwin.pyw
 
 @if %errorlevel% EQU 0 goto continue
 @pause
@@ -9,9 +9,11 @@ C:\Python27\Scripts\pyinstaller -p lib;gfx ^
 
 mkdir dist\nameguiwin\lib\
 mkdir dist\nameguiwin\gfx\
+mkdir dist\nameguiwin\src\
 
 xcopy lib dist\nameguiwin\lib /s /e /h /y
 xcopy gfx dist\nameguiwin\gfx /s /e /h /y
+xcopy src dist\nameguiwin\src /s /e /h /y
 
 rmdir dist\nameguiwin\_MEI\tk\images /s /q
 rmdir dist\nameguiwin\_MEI\tcl\tzdata /s /q
